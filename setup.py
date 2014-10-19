07d779cd571278e40c32f23268fd22739d714978
Hamid Nazari <hn@linux.com>
2014-10-20 01:57:36 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(name='ostruct',
+      version='0.1',
+      description='OpenStruct Data Structure',
+      long_description='OpenStruct, the felixble data structure.',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Development Status :: 4 - Beta',
+      ],
+      keywords=[
+          'ostruct',
+          'openstruct',
+          'data structure',
+          'data structures',
+      ],
+      author='Hamid Nazari',
+      author_email='hn@linux.com',
+      url='http://github.com/hamidnazari/python-ostruct',
+      license='MIT',
+      packages=['ostruct'],
+      zip_safe=False)
